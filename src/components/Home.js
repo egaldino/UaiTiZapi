7dd803ae0795ad648f6cee0a5b97a94e6c192fe0
Edson Galdino da Silva Alves <edson1galdino@gmail.com>
2019-05-04 11:41:41 -0300

    incluindo react-native-tab-view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,47 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
 
 class Home extends Component {
-    render(){
-        return (
-            <View style={{flex: 1, padding: 10, marginTop: 100}}>
-                <Text>Tela Principal do App</Text>
-            </View>
-        )
-    }
+  state = {
+    index: 0,
+    routes: [
+      { key: '1', title: 'Mensagens' },
+      { key: '2', title: 'Contatos' },
+    ],
+  };
+
+  _handleChangeTab = index => this.setState({index});
+
+  _renderHeader = props => <TabBar {...props}/>;
+
+  render() {
+    return (
+      <TabViewAnimated
+        style={styles.scene}
+        navigationState={this.state}
+        renderScene={SceneMap({
+          '1': FirstRoute,
+          '2': SecondRoute,
+        })}
+        renderHeader={this._renderHeader}
+        onRequestChangeTab={this._handleChangeTab}
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
+});
 
 export {Home};
